1cc364703250c148535d18e40150a46e92d40476
Cesco <cescosgames@gmail.com>
2026-07-26 13:24:52 -0400

    pushing debug panel to find nav issue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 	import AddControl from '$lib/components/AddControl.svelte';
 	import Toast from '$lib/components/Toast.svelte';
 	import SettingsButton from '$lib/components/SettingsButton.svelte';
+	import DebugOverlay from '$lib/components/DebugOverlay.svelte';
 
 	let { children } = $props();
 </script>
@@ -17,3 +18,4 @@
 <AddControl />
 <Toast />
 <SettingsButton />
+<DebugOverlay />
